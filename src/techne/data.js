a3c36e47b6702c93b4a2b6d8944adb1d9699f117
David Iacoviello <diacovmusic@gmail.com>
2026-07-29 14:47:29 -0400

    Update resume URL in data.js

--- src/techne/data.js
+++ src/techne/data.js
@@ -9,7 +9,7 @@ export const profile = {
   roles: ['a Developer', 'a Musician', 'an Educator', 'a Lifelong Learner'],
   tagline: 'Full-stack web developer with a musician\'s ear for detail.',
   resumeUrl:
-    'https://drive.google.com/file/d/1tlf4xHOIqUR-00Z-0ZaPUIoPUPSlD0QF/view?usp=sharing',
+    'https://drive.google.com/file/d/1yh9wotG4-t3atBriCtTsy9U2qVohz5ab/view?usp=sharing',
 }
 
 export const social = [
